aedd05fa95e51b5f12a0dcdcfee114fe4c85fc1d
Matt <matthiasvstephens@gmail.com>
2021-10-29 22:57:58 +0200

    oops my bad

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const mainRoutes = require('./mainpage');
 
 router.use('/api', apiRoutes);
+router.use('/', mainRoutes);
 
 module.exports = router;
---
+++ controllers/mainpage.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Project, User } = require('../models');
+
+router.route('/').get(async (req, res) => {
+  try {
+    const data = await Project.findAll({
+      include: [{ model: User, attributes: ['id', 'name'] }],
+    });
+    const projects = data.map((proj) => {
+      return proj.get({ plain: true });
+    });
+    res.render('homepage', { projects, logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(418).json(err);
+  }
+});
+
+router.get('/project/:id', async (req, res) => {
+  try {
+    const data = await Project.findByPk(req.params.id, {
+      include: [{ model: User, attributes: ['id', 'name'] }],
+    });
+    const project = data.get({ plain: true });
+    res.render('project', { project });
+  } catch (err) {
+    res.status(418).json(err);
+  }
+});
+
+router.get('/login', async (req, res) => {
+  try {
+    res.render('login', { logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(418).json(err);
+  }
+});
+
+module.exports = router;
