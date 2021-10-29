639db0832108f4e7dc2492766d2bfb100a0de10f
Andres Liu <andres.liu22@gmail.com>
2021-10-29 14:05:45 -0700

    Add profile route

--- controllers/mainpage.js
+++ controllers/mainpage.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Project, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.route('/').get(async (req, res) => {
   try {
@@ -30,6 +31,21 @@ router.get('/project/:id', async (req, res) => {
 router.get('/login', async (req, res) => {
   try {
     res.render('login', { logged_in: req.session.logged_in });
+  } catch (err) {
+    res.status(418).json(err);
+  }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    const data = await User.findByPk(req.session.user_id, {
+      include: [{ model: Project }],
+      attributes: {
+        exclude: ['password']
+      }
+    })
+    const user = data.get({ plain: true});
+    res.render('user', { user });
   } catch (err) {
     res.status(418).json(err);
   }
